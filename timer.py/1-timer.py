7101d842d1e34e5b21e51bfc0b8cf27783c924aa
Swanjira <sylviahwanjira01@gmail.com>
2023-03-30 01:13:34 -0700

    Timer trial 1

---
+++ timer.py/1-timer.py
@@ -0,0 +1,13 @@
+import time
+
+my_time = int(input("Enter time in seconds"))
+
+for x inrange(my_time, 0, -1);
+seconds = n % 60
+minutes = int(n / 60 ) % 60
+hours = int(n / 3600)
+print(f"{hours: 02}:{minutes: 02}:{seconds: 02})
+        time.sleep(1)
+
+        print("TIME IS UP!")
+
